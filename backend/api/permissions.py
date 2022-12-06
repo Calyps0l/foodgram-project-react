e1cc280a6a35ced72d3a8c426aff51804962b926
Александр Сивачев <calyps0l19@yandex.ru>
2022-12-06 19:15:40 +0300

    permissions

---
+++ backend/api/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class AuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user)
