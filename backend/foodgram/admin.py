c133df2b4f2715a2cfac70239d4e21b09740aafd
Александр Сивачев <calyps0l19@yandex.ru>
2022-12-17 18:29:48 +0300

    isort admin

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import (Favorite, Ingredient, IngredientInRecipe, Recipe, ShoppingCart, Tag)
+from .models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
+                     ShoppingCart, Tag)
 
 
 @admin.register(Ingredient)
